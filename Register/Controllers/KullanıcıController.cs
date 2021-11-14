00e50cb1b002257e076feecb9ac670b4cb26ba51
mbask <mbask@LAPTOP-AGENT6CH>
2021-11-14 15:57:43 +0300

    Add project files.

---
+++ Register/Controllers/KullanıcıController.cs
@@ -0,0 +1,42 @@
+﻿using Register.Entity;
+using Register.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Register.Controllers
+{
+    public class KullanıcıController : Controller
+    {
+        // GET: Kullanıcı
+        [HttpGet]
+        public ActionResult Kaydol()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Kaydol(RegisterViewModel model)
+        {
+            Kullanıcılar newkullanici = new Kullanıcılar { Ad = model.Ad, Soyad = model.Soyad, Id = model.Id, KullanıcıAdı = model.KullaniciAdi, Mail = model.Mail, Telefon = model.Telefon, IsActive = true, IsDelete = true, IsOnline = true, KayitTarihi = DateTime.Now, Sifre = model.Sifre };
+            int basaridurumu = 0;
+            using (KullaniciEntities1 context=new KullaniciEntities1())
+            {
+                context.Kullanıcılar.Add(newkullanici);
+                basaridurumu = context.SaveChanges();
+            }
+            if (basaridurumu > 0)
+            {
+                TempData["Message"] = "Kayıt Başarılı.Bilgiler veritabanına eklendi.";
+            }
+            else
+            {
+                TempData["Message"] = "Kayıt başarısız.";
+
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Register/Models/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Register.Models
+{
+    public class RegisterViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Ad { get; set; }
+        [Required]
+        public string Soyad { get; set; }
+        [Required]
+        [MinLength(5)]
+        public string KullaniciAdi { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Mail { get; set; }
+        public string Telefon { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Sifre { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Sifre",ErrorMessage ="Şifreler Uyuşmuyor.")]
+        public string SifreOnay { get; set; }
+    }
+}
